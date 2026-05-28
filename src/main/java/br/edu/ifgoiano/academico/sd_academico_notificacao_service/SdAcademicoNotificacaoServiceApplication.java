0eb3c1ebbbae81ade2a98479e9f328544331e015
Artur Duarte Monteiro <monteiroarturduarte@gmail.com>
2026-05-28 09:42:02 -0300

    Initial commit

---
+++ src/main/java/br/edu/ifgoiano/academico/sd_academico_notificacao_service/SdAcademicoNotificacaoServiceApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ifgoiano.academico.sd_academico_notificacao_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdAcademicoNotificacaoServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SdAcademicoNotificacaoServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/edu/ifgoiano/academico/sd_academico_notificacao_service/SdAcademicoNotificacaoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ifgoiano.academico.sd_academico_notificacao_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SdAcademicoNotificacaoServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
